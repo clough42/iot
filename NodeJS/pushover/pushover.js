3733571cdb6f0a79c70d18cc93d053c73719595d
clough <james.clough@hp.com>
2016-07-11 22:24:19 -0700

    Pushover integration script.

---
+++ NodeJS/pushover/pushover.js
@@ -0,0 +1,40 @@
+/**
+ * Listen to garage door and publish results to Pushover
+ */
+
+const mqtt = require("mqtt")
+const request = require("request")
+
+const client = mqtt.connect("mqtt://mqtt.clough.local")
+
+function pushover(message) {
+    request.post(
+        'https://api.pushover.net/1/messages.json',
+        { form: {
+            token: process.env.PUSHOVER_TOKEN,
+            user: process.env.PUSHOVER_USER,
+            message: message
+        } },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+        }
+    );
+}
+
+client.on('connect', function() {
+    client.subscribe('sensor/10488302/value')
+})
+
+client.on('message', function(topic, message) {
+    jsmessage = JSON.parse(message.toString())
+    console.log("Message received: " + jsmessage.value)
+
+    var doorState = "open"
+    if( jsmessage.value == 0 ) {
+        doorState = "closed"
+    }
+    pushover("The garage door is " + doorState + ".")
+})
+
