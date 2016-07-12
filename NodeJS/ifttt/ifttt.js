52cbd8c70e3ad399d2d0fcdab8fbfc39c03bb6dd
clough <james.clough@hp.com>
2016-07-12 15:28:30 -0700

    Add error handling to ifttt

--- NodeJS/ifttt/ifttt.js
+++ NodeJS/ifttt/ifttt.js
@@ -2,35 +2,40 @@
  * Listen to garage door and publish results to Pushover
  */
 
-const mqtt = require("mqtt")
-const request = require("request")
+const mqtt = require("mqtt");
+const request = require("request");
 
-console.log("Broker: " + process.env.MQTT_BROKER)
-const client = mqtt.connect(process.env.MQTT_BROKER)
-
-console.log("Token: " + process.env.IFTTT_MAKER_TOKEN)
+console.log("Broker: " + process.env.MQTT_BROKER);
+console.log("Token: " + process.env.IFTTT_MAKER_TOKEN);
 
 function ifttt(sensorid, value) {
     request.post(
         'https://maker.ifttt.com/trigger/' + sensorid + '/with/key/' + process.env.IFTTT_MAKER_TOKEN,
         { form: {
-                value1: value
+            value1: value
         } },
         function (error, response, body) {
-            console.log("Body: " + body)
+            console.log("Body: " + body);
         }
     );
 }
 
+const client = mqtt.connect(process.env.MQTT_BROKER);
+
 client.on('connect', function() {
-    client.subscribe('sensor/+/value')
-})
+    client.subscribe('sensor/+/value');
+});
 
 client.on('message', function(topic, message) {
-    var sensorid = topic.split("/")[1]
-    jsmessage = JSON.parse(message.toString())
-    var value = jsmessage.value
-    console.log("Message: sensor=" + sensorid + ", value=" + value)
-    ifttt(sensorid, value)
-})
+    try {
+        var sensorid = topic.split("/")[1];
+        jsmessage = JSON.parse(message.toString());
+        var value = jsmessage.value;
+        console.log("Message: sensor=" + sensorid + ", value=" + value);
+        ifttt(sensorid, value);
+    }
+    catch(err) {
+        console.log("Caught error: " + err.stack);
+    }
+});
 
