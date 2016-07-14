e14d742508ef141dfda529c71c044cdb9cf2f060
clough <james.clough@hp.com>
2016-07-14 16:45:17 -0700

    Swtch mqtt-mysql to use bluebird promises.

--- NodeJS/mqtt-mysql/mqtt-mysql.js
+++ NodeJS/mqtt-mysql/mqtt-mysql.js
@@ -2,80 +2,77 @@
  * Listen to all sensor messages and publish them to IFTTT
  */
 
+const Promise = require("bluebird");
 const mqtt = require("mqtt");
 const mysql = require("mysql");
 
-console.log("Broker: " + process.env.MQTT_BROKER);
 
-var dbconnection = mysql.createConnection({
+function updatesensor(sensorid, message) {
+    dbQuery(
+        'INSERT INTO sensor (chipid, mac, hostname, ip, status) ' +
+        'VALUES (?, ?, ?, ?, ?) ' +
+        'ON DUPLICATE KEY UPDATE mac = VALUES(mac), hostname = VALUES(hostname), ip = VALUES(ip), status = VALUES(status)',
+        [message.chipid, message.mac, message.hostname, message.ip, message.status]
+    ).then(function(results, fields) {
+        console.log("Saved status for sensor " + sensorid + ": " + message.status);
+    }).catch(function(e) {
+        console.log("Database error: " + e.stack);
+    });
+}
+
+function storesensorvalue(sensorid, message) {
+    dbQuery(
+        'INSERT INTO sensorvalue (chipid, value) VALUES (?, ?)',
+        [sensorid, message.value]
+    ).then(function(results, fields) {
+        console.log("Saved value for sensor " + sensorid + ": " + message.value);
+    }).catch(function(e) {
+        console.log("Database error: " + e.stack);
+    });
+}
+
+function handleMessage(topic, message) {
+    try {
+        var topicParts = topic.split("/")
+        var sensorId = topicParts[1];
+        var messageType = topicParts[2];
+        var parsedMessage = JSON.parse(message.toString());
+
+        switch (messageType) {
+            case "value":
+                console.log("Got value for sensor " + sensorId);
+                storesensorvalue(sensorId, parsedMessage);
+                break;
+            case "status":
+                console.log("Got status for sensor " + sensorId);
+                updatesensor(sensorId, parsedMessage);
+                break;
+        }
+    }
+    catch (e) {
+        console.log("Caught error handling message: " + e.stack);
+    }
+}
+
+
+var dbConnection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 });
+var dbConnect = Promise.promisify(dbConnection.connect, {context: dbConnection});
+var dbQuery = Promise.promisify(dbConnection.query, {context: dbConnection});
 
-function updatesensor(sensorid, message) {
-    console.log("Saving status for sensor " + sensorid + ": " + message.status)    ;
-    dbconnection.query(
-        'INSERT INTO sensor (chipid, mac, hostname, ip, status) ' +
-        'VALUES (?, ?, ?, ?, ?) ' +
-        'ON DUPLICATE KEY UPDATE mac = VALUES(mac), hostname = VALUES(hostname), ip = VALUES(ip), status = VALUES(status)',
-        [message.chipid, message.mac, message.hostname, message.ip, message.status],
-        function (error, results, fields) {
-            if( error ) {
-                console.log("Database error: " + error.stack);
-            }
-        }
-    );
-}
-
-function storesensorvalue(sensorid, message) {
-    console.log("Saving value of sensor " + sensorid + ": " + message.value);
-    dbconnection.query('INSERT INTO sensorvalue (chipid, value) VALUES (?, ?)', [sensorid, message.value], function (error, results, fields) {
-        if( error ) {
-            console.log("Database error: " + error.stack);
-        }
-    });
-}
-
-dbconnection.connect(function(err) {
-    if( err ) {
-        console.log("Error connecting to database: " + err.stack);
-    }
-    else {
-        console.log("Database connected.");
-
-        const client = mqtt.connect(process.env.MQTT_BROKER);
-
-        client.on('connect', function () {
-            client.subscribe('sensor/+/value');
-            client.subscribe('sensor/+/status');
-        });
-
-        client.on('message', function (topic, message) {
-            try {
-                var topicparts = topic.split("/")
-                var sensorid = topicparts[1];
-                var messagetype = topicparts[2];
-                jsmessage = JSON.parse(message.toString());
-
-                switch (messagetype) {
-                    case "value":
-                        console.log("Got value for sensor " + sensorid);
-                        storesensorvalue(sensorid, jsmessage);
-                        break;
-                    case "status":
-                        console.log("Got status for sensor " + sensorid);
-                        updatesensor(sensorid, jsmessage);
-                        break;
-                }
-            }
-            catch (err) {
-                console.log("Caught error: " + err.stack);
-            }
-        });
-    }
+dbConnect().then(function(result) {
+    mqtt.connect(process.env.MQTT_BROKER)
+        .subscribe('sensor/+/value')
+        .subscribe('sensor/+/status')
+        .on('message', handleMessage);
+}).catch(function(e) {
+    console.log("Error connecting to database: " + e.stack);
 });
+
 
 
 
