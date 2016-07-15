98bf818c392edfb065cb5d72402031633f66fac6
clough <james.clough@hp.com>
2016-07-15 06:42:07 -0700

    Switch to use db connection pool to gracefully handle creating new connections when they are closed by the server.

--- NodeJS/mqtt-mysql/mqtt-mysql.js
+++ NodeJS/mqtt-mysql/mqtt-mysql.js
@@ -2,39 +2,55 @@
  * Listen to all sensor messages and publish them to IFTTT
  */
 
-const Promise = require("bluebird");
 const mqtt = require("mqtt");
 const mysql = require("promise-mysql");
 
-var dbConnection;
+const dbConfig = {
+    connectionLimit : 10,
+    host            : process.env.MYSQL_HOST,
+    user            : process.env.MYSQL_USER,
+    password        : process.env.MYSQL_PASSWORD,
+    database        : process.env.MYSQL_DATABASE
+};
 
-function updatesensor(sensorid, message) {
-    dbConnection.query(
-        'INSERT INTO sensor (chipid, mac, hostname, ip, status) ' +
-        'VALUES (?, ?, ?, ?, ?) ' +
-        'ON DUPLICATE KEY UPDATE mac = VALUES(mac), hostname = VALUES(hostname), ip = VALUES(ip), status = VALUES(status)',
-        [message.chipid, message.mac, message.hostname, message.ip, message.status]
-    ).then(function(results, fields) {
-        console.log("Saved status for sensor " + sensorid + ": " + message.status);
-    }).catch(function(e) {
-        console.log("Database error: ", e);
-    });
+var dbPool;
+
+
+function updateSensor(sensorid, message) {
+    dbPool
+        .query(
+            'INSERT INTO sensor (chipid, mac, hostname, ip, status) ' +
+            'VALUES (?, ?, ?, ?, ?) ' +
+            'ON DUPLICATE KEY UPDATE mac = VALUES(mac), hostname = VALUES(hostname), ip = VALUES(ip), status = VALUES(status)',
+            [message.chipid, message.mac, message.hostname, message.ip, message.status]
+        )
+        .then(function(results, fields) {
+            console.log("Saved status for sensor " + sensorid + ": " + message.status);
+        })
+        .catch(function(e) {
+            console.log("Database error: ", e);
+        })
 }
 
-function storesensorvalue(sensorid, message) {
-    dbConnection.query(
-        'INSERT INTO sensorvalue (chipid, value) VALUES (?, ?)',
-        [sensorid, message.value]
-    ).then(function(results, fields) {
-        console.log("Saved value for sensor " + sensorid + ": " + message.value);
-    }).catch(function(e) {
-        console.log("Database error: ", e);
-    });
+
+function storeSensorvalue(sensorid, message) {
+    dbPool
+        .query(
+            'INSERT INTO sensorvalue (chipid, value) VALUES (?, ?)',
+            [sensorid, message.value]
+        )
+        .then(function(results, fields) {
+            console.log("Saved value for sensor " + sensorid + ": " + message.value);
+        })
+        .catch(function(e) {
+            console.log("Database error: ", e);
+        });
 }
+
 
 function handleMessage(topic, message) {
     try {
-        var topicParts = topic.split("/")
+        var topicParts = topic.split("/");
         var sensorId = topicParts[1];
         var messageType = topicParts[2];
         var parsedMessage = JSON.parse(message.toString());
@@ -42,11 +58,11 @@ function handleMessage(topic, message) {
         switch (messageType) {
             case "value":
                 console.log("Got value for sensor " + sensorId);
-                storesensorvalue(sensorId, parsedMessage);
+                storeSensorvalue(sensorId, parsedMessage);
                 break;
             case "status":
                 console.log("Got status for sensor " + sensorId);
-                updatesensor(sensorId, parsedMessage);
+                updateSensor(sensorId, parsedMessage);
                 break;
         }
     }
@@ -56,21 +72,15 @@ function handleMessage(topic, message) {
 }
 
 
-mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).then(function(conn) {
-    dbConnection = conn;
-}).then(function() {
-    mqtt.connect(process.env.MQTT_BROKER)
-        .subscribe('sensor/+/value')
-        .subscribe('sensor/+/status')
-        .on('message', handleMessage);
-}).catch(function(e) {
-    console.log("Error connecting to database: ", e);
-});
+// Create the database pool
+dbPool = mysql.createPool(dbConfig);
+
+// Connect to the broker and subscribe for messages
+mqtt.connect(process.env.MQTT_BROKER)
+    .subscribe('sensor/+/value')
+    .subscribe('sensor/+/status')
+    .on('message', handleMessage);
+
 
 
 
